b83ba58ab074aee0433d7462fdb61f1d17cc76f7
Pankaj <pankaj.phour70@gmail.com>
2023-10-02 15:39:36 +0530

    Working on upload page

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
-
+progress:number = 0;
+uploading:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleInput(e:any){
+    console.log(e);
+    
   }
 
 }
